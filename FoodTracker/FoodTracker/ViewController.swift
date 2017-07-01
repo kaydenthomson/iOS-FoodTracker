29165fa8cd09a0ed4fcabf0e2ae2c66a60a97ce6
kaydenthomson <kaydenthomson@gmail.com>
2017-06-30 23:29:51 -0600

    Initialized

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kayden Thomson on 6/30/17.
+//  Copyright © 2017 Kayden Thomson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
