a86045ca348616dd213c7a4c55182c0f12a81e48
Kayden Thomson <kaydenthomson@gmail.com>
2017-07-03 09:53:42 -0600

    Connect UI to view controller. Can modify a label via a text field or us a button to set to a default text.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nameTextField.delegate = self
     }
-
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
